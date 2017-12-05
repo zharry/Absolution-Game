4fd80e8b362d1730bf871bde6eee4f55e95dcc79
Harry Zhang <zh.harry@yahoo.ca>
2017-12-04 21:33:58 -0500

    Asset Relocation

---
+++ src/GameObject/CheckPoint.java
@@ -0,0 +1,38 @@
+package GameObject;
+
+import java.awt.Graphics;
+
+public class CheckPoint extends GameObject {
+
+	private static final long serialVersionUID = -3234320293018133982L;
+
+	// True = Start
+	// False = End Point
+	private boolean state;
+
+	public CheckPoint(int x, int y, boolean start) {
+		super(x, y, 0, 0, 0, 0);
+		state = start;
+	}
+
+	public boolean isStart() {
+		return state;
+	}
+
+	public int[] getPos() {
+		return new int[] { x, y };
+	}
+
+	@Override
+	public void tick() {
+	}
+
+	@Override
+	public void render(Graphics g) {
+	}
+
+	@Override
+	public void onCollide() {
+	}
+
+}
